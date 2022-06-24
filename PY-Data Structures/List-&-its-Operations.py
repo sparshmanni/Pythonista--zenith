74b4cb53dba80f93b9fa710005f411cd48047f5c
Sparsh Manni <sprshmanni@gmail.com>
2022-06-24 23:07:20 +0530

    Basic

---
+++ PY-Data Structures/List-&-its-Operations.py
@@ -0,0 +1,22 @@
+#creating list in python
+List = [1, 2, 3, "GFG", 2.3]
+print("first list ->",List)
+
+
+# Creating a List with use of multiple values
+List = ["PY", "th", "0N"]
+print("\nList containing multiple values: ")
+print(List)
+
+# accessing a element from the list using indexing
+print("Accessing element from the list")
+print(List[1])
+print(List[2])
+
+# accessing a element using negative indexing
+print("Accessing element using negative indexing")  
+# print the last element of list
+print(List[-1])      
+# print the third last element of list
+print(List[-3])
+
