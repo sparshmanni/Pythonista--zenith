35eec14e83d667936d5211e2826b3781ac04cd5f
Sparsh Manni <sprshmanni@gmail.com>
2022-06-26 13:36:53 +0530

    Basic

---
+++ PY-Data Structures/4_String-&-its-Operations.py
@@ -0,0 +1,19 @@
+String = "Welcome to Python"
+
+print("string is ->",String) 
+      
+# Printing First character 
+print("\nFirst character of String is: ") 
+print(String[0]) 
+      
+# Printing Last character 
+print("\nLast character of String is: ") 
+print(String[-1])
+
+print("")
+
+
+#split() seperates the string having space inbetween
+print("normal string is AS as")
+string1="AS as"
+print("string after spliting ->",string1.split())
