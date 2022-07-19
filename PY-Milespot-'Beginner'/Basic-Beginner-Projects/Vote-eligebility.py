69880cbf602786ad4e92c277e48d6344cc2601d4
Sparsh Manni <sprshmanni@gmail.com>
2022-07-19 16:15:02 +0530

    Basic

---
+++ PY-Milespot-'Beginner'/Basic-Beginner-Projects/Vote-eligebility.py
@@ -0,0 +1,12 @@
+
+##working with Conditions is covered in this 
+
+name=input("ENTER YOUR NAME_")
+
+age=int(input("ENTER YOUR AGE_"))
+
+if age>=18:
+    print("HELLO ",name," YOU ARE ELIGIBLE FOR VOTE")
+else:
+    
+    print("HELLO ",name," YOU ARE NOT ELIGIBLE FOR VOTE")
