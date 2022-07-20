a9caec93188645cc282df7e00caf158bb6577044
Sparsh Manni <sprshmanni@gmail.com>
2022-07-20 22:10:03 +0530

    Basic

---
+++ PY-Data Structures/5_Sets-&-its-Operations.py
@@ -0,0 +1,22 @@
+
+
+my_set = {1, 2, 3, 4, 5, 5, 5} #create set
+print(my_set)
+
+
+my_set = {1, 2, 3}
+my_set.add(4)                  #add element to set
+print(my_set)
+
+
+
+#Opreations 
+
+my_set = {1, 2, 3, 4}
+my_set_2 = {3, 4, 5, 6}
+print(my_set.union(my_set_2), '----------', my_set | my_set_2)
+print(my_set.intersection(my_set_2), '----------', my_set & my_set_2)
+print(my_set.difference(my_set_2), '----------', my_set - my_set_2)
+print(my_set.symmetric_difference(my_set_2), '----------', my_set ^ my_set_2)
+my_set.clear()
+print(my_set)
